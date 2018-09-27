8702f5673acd99c48f6edbb35a091b8f8cb64def
David Teles <contacto@davidteles.com>
2018-09-27 12:28:25 +0100

    Some code

--- Arduino/Basic_Control/Basic_Control.ino
+++ Arduino/Basic_Control/Basic_Control.ino
@@ -5,16 +5,38 @@
 //ALL RIGHTS RESERVED
 
 
-
-
+int luminaire = 9;
+int sensor = A0;
 
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(luminaire, OUTPUT);
+  pinMode(sensor, INPUT);
+  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(sensor);
+  Serial.print("LDR Output");
+  Serial.println(sensorValue);
+  float lux = map(sensorValue, 0, 1023, 0, 100);//Valores nao confirmados pelo datasheet nem calibrados
+  Serial.print("Lux:");
+  Serial.println(lux);
+  int brightness = calculateBrightness(lux);
+  analogWrite(luminaire, brightness);
+  Serial.print("Modulated Output");
+  Serial.println(brightness);
+}
+
+
+
+
+int calculateBrightness(float lux){
+
+  //implementar um controlador que altera o intenssidade do led conforme a iluminaçao
+
 
 }
+
