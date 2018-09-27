7f700ff0bf014ca12bb14e57bbd073c362d49e00
David Teles <contacto@davidteles.com>
2018-09-27 12:39:54 +0100

    Very basic code but should do something

--- Arduino/Basic_Control/Basic_Control.ino
+++ Arduino/Basic_Control/Basic_Control.ino
@@ -7,6 +7,10 @@
 
 int luminaire = 9;
 int sensor = A0;
+int brightness;
+float lastLux;
+int lowLux=200;
+int highLux=500;
 
 
 void setup() {
@@ -24,19 +28,41 @@ void loop() {
   float lux = map(sensorValue, 0, 1023, 0, 100);//Valores nao confirmados pelo datasheet nem calibrados
   Serial.print("Lux:");
   Serial.println(lux);
-  int brightness = calculateBrightness(lux);
-  analogWrite(luminaire, brightness);
+  brightness = calculateBrightness(lux, true);
+  analogWrite(luminaire, map(brightness, 0, 100, 0, 255));
   Serial.print("Modulated Output");
   Serial.println(brightness);
+  delay(50);
 }
 
 
 
 
-int calculateBrightness(float lux){
+int calculateBrightness(float lux, bool ocupation){
+  int output;
+  int luxError;
+  if(ocupation == true){
+    luxError=abs(lux-highLux);
+    
+
+    
+  } else {
+    luxError=abs(lux-lowLux);
+    
+
+
+  
+  }
+
+    if(brightness!=0){
+       output = brightness * (1+(luxError/100));
+
+    } else {
+      output = 10 * (1+(luxError/100));
+    }
 
   //implementar um controlador que altera o intenssidade do led conforme a iluminaçao
-
+  return output;
 
 }
 
