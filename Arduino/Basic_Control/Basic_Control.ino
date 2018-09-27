c178cf2094c4d1dc3a9260897a71f5873f0daa96
David Teles <contacto@davidteles.com>
2018-09-27 15:26:43 +0100

    Basic value handling

--- Arduino/Basic_Control/Basic_Control.ino
+++ Arduino/Basic_Control/Basic_Control.ino
@@ -1,5 +1,5 @@
 //Arduino code for smart luminaire
-//SDCTR 1S 2018/19
+//SCDTR 1S 2018/19
 //Tecnico Lisboa
 //David Teles-Jose Coelho-Afonso Soares
 //ALL RIGHTS RESERVED
@@ -11,58 +11,79 @@ int brightness;
 float lastLux;
 int lowLux=200;
 int highLux=500;
+int counter = 0;
+float acc_value = 0;
 
 
 void setup() {
   Serial.begin(9600);
   pinMode(luminaire, OUTPUT);
   pinMode(sensor, INPUT);
-  
-
+  counter = 0;
+  acc_value = 0;
 }
 
 void loop() {
   int sensorValue = analogRead(sensor);
-  Serial.print("LDR Output");
+  Serial.print("LDR raw output: ");
   Serial.println(sensorValue);
-  float lux = map(sensorValue, 0, 1023, 0, 100);//Valores nao confirmados pelo datasheet nem calibrados
+  acc_value += sensorValue;
+  counter++;
+  Serial.print("LDR raw avg output: ");
+  Serial.println(acc_value / counter);
+
+  // TELES
+  // 833 (adc) - 229 lux
+  // 880 (adc) - 261 lux
+  // 940 (adc) - 305 lux
+  // 383 (adc) - X
+
+
+  // COELHADAS
+  // 360 (adc) - 5 lux
+  // 830 (adc) - 264 lux
+  // 890 (adc) - 1320 lux
+
+/*
   Serial.print("Lux:");
   Serial.println(lux);
-  brightness = calculateBrightness(lux, true);
-  analogWrite(luminaire, map(brightness, 0, 100, 0, 255));
-  Serial.print("Modulated Output");
+  brightness = lux; //100;//calculateBrightness(lux, true);
+  analogWrite(luminaire, map(brightness, 0, 100, 255, 0));
+  Serial.print("Modulated Output: ");
   Serial.println(brightness);
-  delay(50);
+ */
+  delay(500);
 }
 
-
-
+int calculateLux(int ldrReading) {
+  /* Values according to datasheet
+   * 0 lux: 1 MOhm
+   * 1 lux : 60 kOhm
+   * 10 lux : 15 kOhm
+   * 100 lux : 3 kOhm
+   */
+  
+  // float lux = map(sensorValue, 200, 1016, 0, 100); //Valores nao confirmados pelo datasheet nem calibrados
+  // return lux;
+  
+}
 
 int calculateBrightness(float lux, bool ocupation){
   int output;
   int luxError;
-  if(ocupation == true){
+  if (ocupation == true){
     luxError=abs(lux-highLux);
-    
-
-    
   } else {
     luxError=abs(lux-lowLux);
-    
-
-
-  
   }
+  if (brightness!=0){
+     output = brightness * (1+(luxError/100));
 
-    if(brightness!=0){
-       output = brightness * (1+(luxError/100));
-
-    } else {
-      output = 10 * (1+(luxError/100));
-    }
+  } else {
+    output = 10 * (1+(luxError/100));
+  }
 
   //implementar um controlador que altera o intenssidade do led conforme a iluminaçao
   return output;
-
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SCDTR
 Smart luminaire for offices
+
+# Datasheets
+https://cdn.sparkfun.com/datasheets/Sensors/LightImaging/SEN-09088.pdf
