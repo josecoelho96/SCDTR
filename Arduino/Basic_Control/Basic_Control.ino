94db45087a5ac3cef02dae0af5a0429c1544c3d6
David Teles <contacto@davidteles.com>
2018-09-27 12:17:41 +0100

    Basic file added

---
+++ Arduino/Basic_Control/Basic_Control.ino
@@ -0,0 +1,20 @@
+//Arduino code for smart luminaire
+//SDCTR 1S 2018/19
+//Tecnico Lisboa
+//David Teles-Jose Coelho-Afonso Soares
+//ALL RIGHTS RESERVED
+
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
