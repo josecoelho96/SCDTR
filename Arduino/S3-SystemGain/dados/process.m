a2c1d05b327194944b699cfefcfc3c5b204580dc
José Coelho <josecoelho96@gmail.com>
2018-10-18 15:59:40 +0100

    Update matlab script and add data collected

--- Arduino/S3-SystemGain/dados/process.m
+++ Arduino/S3-SystemGain/dados/process.m
@@ -1,8 +1,14 @@
-filename = 't5.tsv';
+filename = 't6-lab.tsv';
 fileID = fopen(filename, 'r', 'n', 'UTF-8');
 
 LDR = [];
 LED = [];
+mLdr = -0.652;
+bLdr = 1.76;
+Vref = 5; % arduino vcc
+maxResistanceLdr = 1000000; % 1MOhm
+R1ref = 9850; % measured value
+
 
 line = fgetl(fileID);
 while ischar(line)
@@ -18,10 +24,58 @@ while ischar(line)
 end
 fclose(fileID);
 
+LED(:, 3) = ((LED(:,2)).*Vref)/255.0;
+
+% LDR content by index
+% 1 - timestamp
+% 2 - adc value
+% 3 - voltage
+% 4 - resistance
+% 5 - lux value
+LDR(:, 3) = (Vref*LDR(:, 2))/1023.0;
+
+for i=1:length(LDR)
+    if LDR(i, 3) == 0
+        LDR(i, 4) = maxResistanceLdr;
+    else
+        LDR(i, 4) = R1ref*(Vref/LDR(i, 3) - 1);
+    end
+end
+
+LDR(:, 5) = 10.^((log10(LDR(:, 4)/1000) - bLdr)/mLdr);
+
+% smoothing out of values
+for i = 1:length(LDR)-5
+    LDRsmooth(i, :) = [LDR(i, 1) mean(LDR(i:i+5, 3))];
+end
+    
 figure;
+hold on;
+grid on;
 yyaxis left;
-plot(LDR(:,1), LDR(:,2))
+stairs(LED(:,1), LED(:,2), 'b')
+ylabel('Input: LED [8-bit PWM: 0 - 255]')
 yyaxis right;
-stairs(LED(:,1), LED(:,2))
+plot(LDR(:,1), LDR(:,2), 'r')
+title('Variação do LED e reposta do LDR - Raw');
+xlabel('Tempo [us]')
+legend('PWM LED', 'Medição ADC LDR');
+
 figure;
-plot(diff(LDR(:,1)))
+hold on;
+grid on;
+yyaxis left;
+stairs(LED(:,1), LED(:,3), 'b.-')
+ylabel('Input: LED [8-bit PWM: 0 - 255]')
+yyaxis right;
+plot(LDR(:,1), LDR(:,3), 'r.--')
+plot(LDRsmooth(:,1), LDRsmooth(:,2), 'g.-')
+% ylabel('Resposta no LDR [Voltage]')
+% title('Variação do LED e reposta do LDR - Processado');
+% xlabel('Tempo [us]')
+% legend('PWM LED', 'Medição Voltage LDR', 'Medição voltage LDR (averaged)');
+
+% adding helper lines
+refline(0, 1.90232);
+refline(0, 3.3418);
+
