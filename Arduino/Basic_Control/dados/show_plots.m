4d738891ede0bc39504764f6e35186efe16addbe
José Coelho <josecoelho96@gmail.com>
2018-10-29 09:52:14 +0000

    Add matlab script to tune PID gains

--- Arduino/Basic_Control/dados/show_plots.m
+++ Arduino/Basic_Control/dados/show_plots.m
@@ -1,4 +1,4 @@
-data = load('t3.tsv');
+data = load('t5.tsv');
 
 Ki = 0.5;
 Kp = 1;
@@ -7,7 +7,7 @@ plot(data(:, 1)/1000000, data(:, 2));
 hold on;
 plot(data(:, 1)/1000000, data(:, 3));
 grid on;
-xlabel('Tempo [s])');
+xlabel('Tempo [s]');
 ylabel('Valor [PWM | lux]');
 title(sprintf('Evolucao temporal do sistema. Kp =  %.2f, Ki = %.2f', Kp, Ki));
 legend('Entrada (PWM)', 'Saída (lux)');
