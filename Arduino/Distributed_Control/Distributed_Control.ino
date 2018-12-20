6a0b519673731669c657219a7355924b36206e49
José Coelho <josecoelho96@gmail.com>
2018-12-20 00:17:15 +0000

    Add part 1 code

--- Arduino/Distributed_Control/Distributed_Control.ino
+++ Arduino/Distributed_Control/Distributed_Control.ino
@@ -7,42 +7,6 @@
 // David Teles - Jose Coelho - Afonso Soares
 // ALL RIGHTS RESERVED
 
-// ============================== LOGGING CONTROL =============================
-// #define DEBUG
-// #define LOOP_INFO
-// #define FEEDBACK_DEBUG
-// #define TIMING_DEBUG
-#define PLOT_SYSTEM
-
-// ============================== LOGGING CONTROL =============================
-
-// ============================== Calibration =============================
-
-volatile float k = 0;
-volatile int number_of_readings = 0;
-// ============================== Calibration =============================
-
-// ============================== Consensus =============================
-
-#define CONSENSUSBYTE 3
-#define TARGETBYTE 4
-int consensus;
-
-// ============================== Consensus =============================
-// ================================== PINOUT ==================================
-const int luminaire = 3;
-const int presenceSensor = 2;
-const int lightSensor = A0;
-// ================================== PINOUT ==================================
-
-
-// ============================== STATE VARIABLES =============================
-volatile bool occupied; // True if presence detected, False otherwise
-volatile int targetLux; // Changed according occupation and LOW_LUX or HIGH_LUX
-volatile bool updateFeedback; // True if feedback parameters should be updated
-// ============================== STATE VARIABLES =============================
-
-
 // ============================= CONTROL VARIABLES ============================
 // const float T = 0.002; // freq: 500 Hz
 const float T = 0.004; // freq: 250 Hz
@@ -59,7 +23,7 @@ const float bLdr = 1.76; // LDR characteristic curve: b parameter
 const float Kp = 1; // Proportional gain
 const float Ki = 0.5; // Integral gain
 // const float Kd = 0; // Derivative gain
-#define WINDUPMAX 160 // Max value for integrator term
+#define WINDUPMAX 80 // Max value for integrator term
 const float mLuxPWMConverter = 0.5519;
 const float bLuxPWMConverter = 1.2202;
 const int bPID = 1;
@@ -72,6 +36,9 @@ const int bPID = 1;
 #define DEADZONE 2
 int controlSignal; // LED PWM
 int controlSignalFeedforward;
+unsigned long lastFeedforwardChange;
+float Vf;
+float Vi;
 float measuredLux;
 float p;
 float i;
@@ -79,15 +46,34 @@ float i_prev;
 float error_prev;
 // ============================= SYSTEM PARAMETERS ============================
 
+
+
+
+
+
+
+// ============================== STATE VARIABLES =============================
+volatile bool occupied; // True if presence detected, False otherwise
+volatile int targetLux; // Changed according occupation and LOW_LUX or HIGH_LUX
+volatile bool updateFeedback; // True if feedback parameters should be updated
+// ============================== STATE VARIABLES =============================
+
+// ================================== PINOUT ==================================
+const int luminaire = 3;
+const int presenceSensor = 2;
+const int lightSensor = A0;
+// ================================== PINOUT ==================================
+
 // ============================== GLOBAL SETTINGS =============================
 const int MAX_NODES = 3; // Defined the max number of nodes
 const int TIMEOUT = 2000; // ms
-const int CALIBRATION_LED_ON_TIME = 2000; //ms
+const int CALIBRATION_LED_ON_TIME_MIN_TIME = 1000; //ms
+const int CALIBRATION_LED_ON_TIME = 3000; //ms
+const int CALIBRATION_LDR_READS = 100;
 // ============================== GLOBAL SETTINGS =============================
 
-
 // =============================== NODE SETTINGS ==============================
-const byte address = 10;
+const byte address = 5;
 // =============================== NODE SETTINGS ==============================
 
 // =================================== FLAGS ==================================
@@ -99,29 +85,36 @@ volatile boolean f_node_already_on_network = false;
 volatile boolean f_send_joined_network = false;
 volatile boolean f_joined_network = false;
 volatile boolean f_in_network = false;
-volatile boolean f_calibration_measure_ldr = false;
 
 volatile byte last_node_led_on = 0;
 volatile boolean f_calibration_mode = false;
 volatile boolean f_calibration_next_light = false;
 volatile boolean f_calibration_led_on = false;
 volatile boolean f_calibration_need_to_light_led_on = false;
-
+volatile boolean f_calibration_measure_ldr = false;
+unsigned long start_time_calibration_led_on_min_time;
 unsigned long start_time_calibration_led_on;
+
+volatile boolean f_setup = true;
+
+
+// ================================ CALIBRATION ===============================
+volatile float k = 0;
+volatile int number_of_readings = 0;
+// ================================ CALIBRATION ===============================
 
 
 // =================================== FLAGS ==================================
 
 
-
 void setup() {
+
+  // Stop all interrupts untill all registers are set
+  noInterrupts();
 
   // Debug only
   Serial.begin(9600);
 
-  pinMode(3, OUTPUT);
-  pinMode(A0, INPUT);
-  
   // Reset all values to defaults
   current_neighbour_nodes = 0;
   for (int i = 0; i < MAX_NODES; i++) {
@@ -138,12 +131,93 @@ void setup() {
   f_calibration_next_light = false;
   f_calibration_led_on = false;
   f_calibration_need_to_light_led_on = false;
+  f_calibration_measure_ldr = false;
+  k = 0;
+  number_of_readings = 0;
 
+  f_setup = true;
+
+
+  // ============================== TIMER CONTROL =============================
+  // ==================== TIMER2 : Faster PWM on pin 3, 11 ====================
+  // Reset TCCR2B register Clock Select (CS) bits
+  // TC2 - Timer/Counter2 (8 bits)
+  // TCCR2B - TC2 Control Register B
+  TCCR2B &= B11111000; // Mask other bits
+  // Setting no prescaler: 001
+  // TCCR2B |= (1 << CS22);
+  // TCCR2B |= (1 << CS21);
+  TCCR2B |= (1 << CS20);
+  // ==================== TIMER2 : Faster PWM on pin 3, 11 ====================
+
+  // ========================= TIMER1 : Feedback loop =========================
+  // Resetting TCCR1A and TCCR1B registers.
+  // TC1 - Timer/Counter1 (16 bits)
+  // TCCR1A - TC1 Control Register A
+  // TCCR1B - TC1 Control Register B
+  TCCR1A = B00000000;
+  TCCR1B = B00000000;
+
+  // Changing mode of operation to CTC Mode (Clear Timer on Compare Match)
+  // WGM - Waveform Generation Mode
+  TCCR1B |= (1 << WGM12);
+
+  // CS - Clock Select
+  // Prescalers
+  // - 1024 : 101
+  // - 256  : 100
+  TCCR1B |= (1 << CS12);
+  // TCCR1B |= (1 << CS11);
+  // TCCR1B |= (1 << CS10);
+
+  // Setting interrupts to be called on counter match with OCR1A.
+  // TIMSK1 - Timer/Counter 1 Interrupt Mask Register
+  // OCIEA - Output Compare A Match Interrupt Enable
+  TIMSK1 |= (1 << OCIE1A);
+
+  // OCR1A: Output Compare Register
+  // Interruptions will occur at each (OCR1A+1)*T seconds
+  // T_interrupt = (OCR+1)*T
+  // f_interrupt = 16MHz /((OCR+1)*prescaler) = f_scaled/(OCR+1)
+  // For 1024 prescaler, T = 64 us, f = 15.625 kHz
+  // For 256 prescaler, T = 16 us, f = 62.5 kHz <---
+
+  // OCR1A = 124; // T = 2 ms | f = 500 Hz
+  OCR1A = 249; // T = 4 ms | f = 250 Hz
+  // OCR1A = 624; // T = 10 ms | f = 100 Hz
+
+  // ========================= TIMER1 : Feedback loop =========================
+  // ============================== TIMER CONTROL =============================
+
+  // ==================================== IO ==================================
+  pinMode(luminaire, OUTPUT);
+  pinMode(lightSensor, INPUT);
+  pinMode(presenceSensor, INPUT_PULLUP);
+  // ==================================== IO ==================================
+
+  // Setup is complete, re-enable interrupts
+  interrupts();
+
+  analogWrite(luminaire, 0);
+
+  // Attach the presence sensor to an interrupt
+  attachInterrupt(digitalPinToInterrupt(presenceSensor), stateChange, CHANGE);
+ 
+  // Check for initial state (occupied or not)
+  stateChange();
+  updateFeedback = false;
+  p = 0;
+  i = 0;
+  measuredLux = 0;
+  i_prev = 0;
+  error_prev = 0;
 
   startI2C();
   if (requestJoinNetwork()) {
     requestForCalibration();
   }
+
+  f_setup = false;
 }
 
 void loop() {
@@ -162,50 +236,34 @@ void loop() {
     calibrate();
   }
 
-  if(f_calibration_measure_ldr=true){
-    k = getLDRLux();
-    number_of_readings++;
-  } else if(k != 0 && number_of_readings != 0){
-    byte reading[4];
-    k=k/number_of_readings;
-    number_of_readings = 0;
-    Wire.beginTransmission(0);
-    Wire.write(address);
-    Wire.write(MT_CALIBRATION_VALUE);
-    Wire.write(4);
-    *((float *) reading) = k;
-    Wire.write(reading,4);
-    Wire.endTransmission(); 
+  if (updateFeedback == true && f_calibration_mode == false) {
+    feedback();
+    updateFeedback = false;
   }
-
   
-  
-    
-  
-  // ================ DEBUG ===================================
-  // Serial.print("L");
-
-  /*
-  Serial.print(current_neighbour_nodes);
-  Serial.print("-");
-  for (int i = 0; i < MAX_NODES; i++) {
-    Serial.print(neighbour_nodes_addresses[i]);
-    if (MAX_NODES - i > 1) {
-      Serial.print(":");
+  if (f_calibration_measure_ldr == true) {
+    if (millis() - start_time_calibration_led_on_min_time > CALIBRATION_LED_ON_TIME_MIN_TIME) {
+      // Add time to get stable
+      if (number_of_readings < CALIBRATION_LDR_READS) {
+        k += getLDRLux();
+        // sendValueFloat(MT_CALIBRATION_VALUE, k);
+        // Serial.println(k);
+        number_of_readings++;
+      } else {
+        sendValueFloat(MT_CALIBRATION_VALUE, k/CALIBRATION_LDR_READS);
+        f_calibration_measure_ldr = false;
+      }
     }
   }
-  */
-  // Serial.print("-");
-  // Serial.print(f_in_network);
-  // Serial.print("-");
-  // Serial.print(f_calibration_mode);
 
-  // Serial.println("|");   
-  // delay(500);
-  // ================ DEBUG ==================================
+  Serial.print("L");
+  Serial.print("-");
+  Serial.print(f_calibration_mode);
+  Serial.print("-");
+  Serial.print(f_setup);
+  Serial.println("-");
+
 }
-
-
 
 void calibrate() {
 
@@ -344,7 +402,9 @@ void receiveData(int howMany) {
   if (header[1] == MT_CALIBRATION_LED_ON) {
     //TODO: Anything?
     f_calibration_measure_ldr = true;
-    
+    number_of_readings = 0;
+    start_time_calibration_led_on_min_time = millis();
+    k = 0;
   }
 
   if (header[1] == MT_CALIBRATION_LED_OFF) {
@@ -366,7 +426,16 @@ void sendData(byte type) {
   Wire.beginTransmission(0);
   Wire.write(address);
   Wire.write(type);
-  Wire.write(0); // TODO: Size!
+  Wire.endTransmission();
+}
+
+void sendValueFloat(byte type, float value) {
+  Wire.beginTransmission(0);
+  Wire.write(address);
+  Wire.write(type);
+  Wire.write(4);
+  byte* data = (byte*)&value;
+  Wire.write(data, 4);
   Wire.endTransmission();
 }
 
@@ -391,9 +460,6 @@ void requestForCalibration() {
   f_calibration_led_on = false;
 
   sendData(MT_REQUEST_FOR_CALIBRATION);
-  // unsigned long start_time = millis();
-  // Block for 2 secons (interrupts will be called)
-  // while (millis() - start_time < TIMEOUT) {}
 }
 
 // Converts an ADC reading (from a LDR) to lux
@@ -404,16 +470,8 @@ float getLDRLux() {
   float luxLdr;
 
   adcLdr = analogRead(lightSensor);
-  #ifdef DEBUG
-    Serial.print("LDR [ADC value]: ");
-    Serial.println(adcLdr);
-  #endif
 
   voltageLdr = (Vref*adcLdr)/1023.0;
-  #ifdef DEBUG
-    Serial.print("LDR [Voltage at terminal]: ");
-    Serial.println(voltageLdr);
-  #endif
 
   if (voltageLdr == 0) {
     // Complete darkness, use value from datasheet
@@ -422,20 +480,167 @@ float getLDRLux() {
     resistanceLdr = R1ref*(Vref/voltageLdr - 1);
   }
 
-  #ifdef DEBUG
-    Serial.print("LDR [Resistance]: ");
-    Serial.println(resistanceLdr);
-  #endif
-
   // Resistance is converted to kOhm
   luxLdr = pow(10, (log10(resistanceLdr/1000) - bLdr)/mLdr);
-
-  #ifdef DEBUG
-    Serial.print("LDR [Luminance]: ");
-    Serial.println(luxLdr);
-  #endif
 
   return luxLdr;
 }
 
+void feedback() {
 
+  float realTargetLux = targetLux;
+  // realTargetLux = simulator();
+
+  float errorRaw; // Error before deadzone filtering
+  float error; // Error after deadzone filtering
+  float u_feedback;
+
+  measuredLux = getLDRLux();
+
+  //Calculate the error between the current lux value and the refence value
+  errorRaw = realTargetLux - measuredLux;
+
+  // Apply deadzone filtering
+  error = deadzone_filtering(errorRaw);
+
+  // PI controller calculations
+  p = Kp * bPID * realTargetLux - Kp * measuredLux;
+  i = i_prev + (Kp * Ki * T/2) * (error + error_prev);
+
+  // Block integrator term (basic integrator-windup solution)
+  if (i > WINDUPMAX) {
+    i = WINDUPMAX;
+  } else if (i < -WINDUPMAX) {
+    i = -WINDUPMAX;
+  }
+
+  // Compute control signal
+  // u = p + i + d - simulator();
+
+  u_feedback = p + i;
+
+  controlSignal = controlSignalFeedforward + u_feedback;
+  if (controlSignal > 255) {
+    controlSignal = 255;
+  } else if (controlSignal < 0) {
+    controlSignal = 0;
+  }
+
+  // Produce output
+  analogWrite(luminaire, controlSignal);
+
+  // Save values for next iteration
+  i_prev = i;
+  error_prev = error;
+}
+
+// Deadzone filtering
+float deadzone_filtering(float error) {
+  // TODO: Which method?
+  // ============================ METHOD 1 (Slides) ===========================
+  // Symmetrical with band equal to zero. No gaps and slope = 1
+  if (abs(error) <= DEADZONE) {
+    return 0;
+  } else if (error < DEADZONE) {
+    return error + DEADZONE;
+  } else if (error > DEADZONE) {
+    return error - DEADZONE;
+  }
+  // ============================ METHOD 1 (Slides) ===========================
+}
+
+void stateChange() {
+  // Update occupied state
+  occupied = !digitalRead(presenceSensor);
+
+  // Update target lux value
+  if (occupied == true) {
+    targetLux = HIGH_LUX;
+  } else {
+    targetLux = LOW_LUX;
+  }
+
+  // Update system directly
+  if (f_calibration_mode == false && f_setup == false) {
+    feedforward();
+  }
+
+}
+
+void feedforward(){
+  int adcLdr= analogRead(lightSensor);
+  float R;
+  Vi = (Vref*adcLdr)/1023.0;
+
+  // Get corresponding PWM value to desired lux
+  controlSignal = luxToPWM(targetLux);
+
+  // R in ohm
+  R = pow(10, mLdr*log10(targetLux) + bLdr);
+  Vf = Vref/((R/R1ref) + 1);
+
+  // Get corresponding PWM value to desired lux
+  controlSignal = luxToPWM(targetLux);
+
+  lastFeedforwardChange = millis();
+  analogWrite(luminaire, controlSignal);
+  controlSignalFeedforward = controlSignal;
+}
+
+// Converts from lux to PWM value, based on lab experiments
+int luxToPWM(float lux) {
+  // lux = pwm * m + b
+  // => PWM = (lux - b) / m
+  int pwm;
+  pwm = (lux - bLuxPWMConverter) / mLuxPWMConverter;
+  //Prevents overflow of the output values
+  if (pwm > 255) {
+    pwm = 255;
+  } else if (pwm < 0) {
+    pwm = 0;
+  }
+  return pwm;
+}
+
+// Simulates the delay caused by the charging of the capacitor
+// Outputs actually expected lux value
+float simulator() {
+  unsigned long current_time = millis();
+  float resistanceLdr;
+  float v_adc;
+
+  v_adc = Vf - (Vf - Vi)*exp((-current_time-lastFeedforwardChange)/time_constant());
+  #ifdef DEBUG_SIMULATOR
+    Serial.print("Vf: ");
+    Serial.println(Vf);
+
+    Serial.print("Vi: ");
+    Serial.println(Vi);
+
+    Serial.print("V_adc: ");
+    Serial.println(v_adc);
+  #endif
+  if (v_adc == 0) {
+    // Complete darkness, use value from datasheet
+    resistanceLdr = maxResistanceLdr;
+  } else {
+    resistanceLdr = R1ref*(Vref/v_adc - 1);
+  }
+
+  // Returns real expected lux value
+  return pow(10, (log10(resistanceLdr/1000) - bLdr)/mLdr);
+}
+
+
+// Outputs tau (the time constant) as a second order approximation
+float time_constant() {
+  float a = 0.008391063263791;
+  float b = 0.029646566281119;
+  float c = 0.021355097710248;
+  return a * pow(2, Vf - Vi) + b * (Vf - Vi) + c;
+}
+
+ISR(TIMER1_COMPA_vect) {
+  // Activate flag
+  updateFeedback = true;
+}
